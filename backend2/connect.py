c14f273b0f6748c737b0aa1844687ae9fb28fd72
Gokul <156660981+GP8-Gokul@users.noreply.github.com>
2025-05-21 11:18:47 +0530

    Update connect.py

--- backend2/connect.py
+++ backend2/connect.py
@@ -2,7 +2,7 @@ import sqlitecloud
 
 def get_cursor():
     global conn 
-    conn = sqlitecloud.connect("sqlitecloud://cgzby8cphz.g1.sqlite.cloud:8860/studyplanner.db?apikey=CKbk1dKMFgJKoiIXCg9lJteo5I1HwUPhqJdvNzbcsGE")
+    conn = sqlitecloud.connect("")
     conn.execute("PRAGMA foreign_keys = ON")
     cursor=conn.cursor()
     return cursor
